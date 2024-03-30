6902b99ffd617c13144aaa7c89009424722b4e79
Yuriy Kulikov <kulikov-y@yandex.ru>
2024-03-30 11:03:51 +0000

    Слайдер работает

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,5 +1,37 @@
 const slider = () => {
-    const slider = 'slider.js'
-    console.log(slider);
+
+    console.log('slider');
+    const slider = document.querySelector('.top-slider')
+
+    const slides = slider.querySelectorAll('.item')
+    console.log(slides);
+    // slides.forEach(elem => {
+    //     elem.style.display = "none"
+
+    // })
+    //Переменная для выбора соответствующего слайда
+    let currentSlide = 0
+
+
+    //Функция автоматического переключения слайдов
+    const autoSlide = () => {
+        slides[currentSlide].style.display = "none";
+        currentSlide++
+
+        if(currentSlide >= slides.length) {
+            currentSlide = 0
+        }
+
+        slides[currentSlide].style.display = "block";
+    }
+
+
+const startSlide = () => {
+    setInterval(autoSlide, 3000)
+
 }
-export default slider
+
+
+startSlide()
+}
+export default slider()
