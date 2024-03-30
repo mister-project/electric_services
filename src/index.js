99fad2fb95830e63316b1daffd0e698a2661e5e7
Yuriy Kulikov <kulikov-y@yandex.ru>
2024-03-30 13:06:01 +0000

    Поправил ошибку в функционале модального окна

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import modal from './modules/modal'
 import modulTwo from './modules/two'
-console.log('index.js');
+
 modal();
 modulTwo();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,16 @@
 //Модуль для вызова и закрытия модального окна
 const modal = () => {
-    console.log('modal')
-    const modal = document.querySelector('.modal-callback')
 
-    const button = document.querySelector('.callback-btn')
+    //Получаем модальное окно целиком 
+    const modal = document.querySelector('.modal-callback')
+    
+    //Получаем кнопку закрытия внутри модального окна
+    const modalClose = modal.querySelector('.modal-close')
+    
+    //Получаем тень целиком (overlay)
     const overlay = document.querySelector('.modal-overlay')
-    const body = document.querySelector('body')
+    
+    //слушаем события во всем документе, через дилегирование отлавливаем  нажатие кнопки классом 'callback-btn' и делаем видимыми модальное окно и тень
 
     document.addEventListener('click', (e) => {
         if (!e.target.closest('.callback-btn')) return;
@@ -15,23 +20,20 @@ const modal = () => {
     })
 
     //Закрываем модальное окно и тень при нажатии на тень
-    body.addEventListener('click', (e) => {
-        if (!e.target.closest('.modal-overlay')) return;
-
+    overlay.addEventListener('click', () => {
+      
         modal.style.display = 'none'
         overlay.style.display = 'none'
     })
+
     //Закрываем модальное окно и тень при нажатии на крестик в самом окне
-    modal.addEventListener('click', (e) => {
+    modalClose.addEventListener('click', () => {
 
-        if (!e.target.classList.contains('modal-close')) {
-            modal.style.display = 'none'
-            overlay.style.display = 'none'
-
-        }
+        modal.style.display = 'none'
+        overlay.style.display = 'none'
     })
 
 }
 
 
-export default modal
+export default modal()
