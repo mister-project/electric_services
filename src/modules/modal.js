1beee2acacf3d35921edcd22ea51e8f5bade8992
Yuriy Kulikov <kulikov-y@ya.ru>
2024-03-31 10:54:53 +0300

    устранил ошибочку-скобка в вызове функции модуля

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,13 +3,13 @@ const modal = () => {
 
     //Получаем модальное окно целиком 
     const modal = document.querySelector('.modal-callback')
-    
+
     //Получаем кнопку закрытия внутри модального окна
     const modalClose = modal.querySelector('.modal-close')
-    
+
     //Получаем тень целиком (overlay)
     const overlay = document.querySelector('.modal-overlay')
-    
+
     //слушаем события во всем документе, через дилегирование отлавливаем  нажатие кнопки классом 'callback-btn' и делаем видимыми модальное окно и тень
 
     document.addEventListener('click', (e) => {
@@ -21,7 +21,7 @@ const modal = () => {
 
     //Закрываем модальное окно и тень при нажатии на тень
     overlay.addEventListener('click', () => {
-      
+
         modal.style.display = 'none'
         overlay.style.display = 'none'
     })
@@ -36,4 +36,4 @@ const modal = () => {
 }
 
 
-export default modal()
+export default modal
