507cb000886c1e9ce4c9bdc0c0a97972faa7eb5b
Yuriy Kulikov <kulikov-y@ya.ru>
2024-04-01 19:40:32 +0300

    Кнопка Up работает подьем наверх

--- src/modules/buttonUp.js
+++ src/modules/buttonUp.js
@@ -3,6 +3,19 @@ const buttonUp = () => {
     const btnUp = document.querySelector('.up')
     console.log(btnUp)
 
+    btnUp.addEventListener('click', () => {
+        goTop()
+    })
+
+    const goTop = () => {
+        console.log('ура')
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -10);
+            setTimeout(goTop, 0)
+        }
+
+    }
+
 }
 
 export default buttonUp
