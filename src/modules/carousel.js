4597dc71bbf5f3cb9a6557830c2695e7144523a8
Yuriy Kulikov <kulikov-y@ya.ru>
2024-04-01 15:33:44 +0300

    Карусель - заработали кастомные кнопки внизу

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,7 +1,32 @@
 import Splide from '@splidejs/splide';
-export const carousel = () => {
-    console.log('carousel')
 
-    new Splide('.services-splide').mount();
-    // services-splide
+export const carousel = () => {
+    //Создаем 2 переменные - под наши стрелки. Для навешивания управления на наши (кастомные) стрелки
+    const leftArrow = document.querySelector('.arrow-left')
+    const rightArrow = document.querySelector('.arrow-right')
+
+
+    const splide = new Splide('.services-splide', {
+        //Ниже - классические настройки самого Splider'a
+        pagination: false, //убираем точки внизу слайдов
+
+        arrows: false, //отключаем кнопки самого Splider'a
+        //Настройки количества слайдов для разных экранов, начиная с Десктопа
+        perPage: 3,
+        breakpoints: {
+            768: {
+                perPage: 2,
+            },
+            640: {
+                perPage: 1,
+            },
+        }
+    }).mount();
+    //Создаем слушателей для ловли нажатия на наши кнопки
+    leftArrow.addEventListener('click', () => {
+        splide.go('-1')
+    })
+    rightArrow.addEventListener('click', () => {
+        splide.go('+1')
+    })
 }
