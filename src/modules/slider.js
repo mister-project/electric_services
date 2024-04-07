d9014061faa3e5322dacd7b1fdfab9bf19084fa3
Yuriy Kulikov <kulikov-y@ya.ru>
2024-04-07 14:19:45 +0300

    слайдер - текст появился

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,123 +1,37 @@
 const slider = () => {
-    //Получение слайдера целиком
-    const topSlider = document.querySelector(".top-slider");
-    //Получение списка слайдов
-    const sliderItems = topSlider.querySelectorAll(".item");
-    //Интервал переключения
-    const timerInterval = 3000;
 
-    let interval;
-    let currentItem = 0;
-    //Получение точек из верстки
-    let dots = topSlider.querySelectorAll(".dot");
+    const slider = document.querySelector('.top-slider')
 
-    //Создаем точки
-    const createDots = () => {
-        const slickDots = document.createElement("ul");
-        slickDots.className = "slick-dots"; //присвоение класса блоку точек
-        topSlider.append(slickDots); //добавляем точки в слайдер
-        console.log(topSlider)
-        return slickDots;
+    const slides = slider.querySelectorAll('.item')
 
-    };
-    //создание точек как 'li' и присвоение классов
-    const addDots = () => {
-        const dotBlock = createDots();
-        for (let i = 0; i < sliderItems.length; i++) {
-            const li = document.createElement("li");
-            if (i === 0) {
-                li.className = "dot slick-active";
-            } else {
-                li.className = "dot";
-            }
-            dotBlock.append(li);
+
+
+    //Переменная для выбора соответствующего слайда
+    let currentSlide = 0
+
+
+    //Функция автоматического переключения слайдов
+    const autoSlide = () => {
+        slides[currentSlide].style.display = "none";
+        slides[currentSlide].querySelector(".table").classList.remove("active");
+        currentSlide++
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0
         }
-    };
 
-    const addEventDots = () => {
-        dots = topSlider.querySelectorAll(".dot");
-        topSlider.addEventListener("click", (e) => {
-            console.log(e.target)
-            if (e.target.matches(".dot")) {
-                dots.forEach((dot, index) => {
-                    if (e.target === dot) {
-                        switchingSlides(index);
-                    }
-                });
-            }
-        });
-    };
+        slides[currentSlide].style.display = "block";
+        slides[currentSlide].querySelector(".table").classList.add("active");
 
-    const prevSlide = (index) => {
-        sliderItems[index].style.display = "none";
-        sliderItems[index].querySelector(".table").classList.remove("active");
-        dots[index].classList.remove("slick-active");
-    };
+    }
 
-    const nextSlide = (index) => {
-        sliderItems[index].style.display = "block";
-        dots[index].classList.add("slick-active");
-        setTimeout(() => {
-            sliderItems[index].querySelector(".table").classList.add("active");
-        }, 200);
-    };
-
-    const getCurrentItem = (index) => {
-        if (index >= sliderItems.length) {
-            currentItem = 0;
-        } else {
-            currentItem++;
-        }
-    };
-
-    const switchingSlides = (index) => {
-        getCurrentItem(currentItem + 1);
-        for (let i = 0; i < sliderItems.length; i++) {
-            if (i === index) {
-                nextSlide(i);
-            } else {
-                prevSlide(i);
-            }
-        }
-    };
-
-    const stopSlide = () => {
-        clearInterval(interval);
-    };
 
     const startSlide = () => {
-        interval = setInterval(() => {
-            switchingSlides(currentItem);
-        }, timerInterval);
-    };
+        setInterval(autoSlide, 3000)
 
-    topSlider.addEventListener(
-        "mouseenter",
-        (e) => {
-            if (e.target.matches(".dot")) {
-                stopSlide();
-            }
-        },
-        true
-    );
+    }
 
-    topSlider.addEventListener(
-        "mouseleave",
-        (e) => {
-            if (e.target.matches(".dot")) {
-                startSlide(timerInterval);
-            }
-        },
-        true
-    );
 
-    const init = () => {
-        addDots();
-        addEventDots();
-        switchingSlides(currentItem);
-        startSlide();
-    };
-
-    init();
-};
-export default slider;
+    startSlide()
+}
+export default slider
