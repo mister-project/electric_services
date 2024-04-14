d6b5615ab29d8e42d48853b7401f2abd6b34052e
Yuriy Kulikov <kulikov-y@ya.ru>
2024-04-14 08:16:52 +0300

    подключаю PHPMailer - загрузил папку плагина

---
+++ dist/server.php
@@ -0,0 +1,9 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exeption.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPmailer(true);
+$mail = CharSet = 'UTF-8';
